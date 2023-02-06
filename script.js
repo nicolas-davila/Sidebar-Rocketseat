8e10105cc52eea9985e9dd4c8f357cb4a87037a4
Nicolas Dávila <123404361+nicolas-davila@users.noreply.github.com>
2023-02-06 19:32:50 +0000

    criando o js

---
+++ script.js
@@ -0,0 +1,7 @@
+function clicou() {
+  const menu = document.querySelector('nav');
+  const botaoLogo = document.querySelector('#imagem')
+
+  botaoLogo.classList.toggle('logo-')
+  menu.classList.toggle('add-nav')
+}
